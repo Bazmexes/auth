dca34c9efed626a00f89bdba4833f2097d08347e
Bazarov Maxim <bazarow.max@gmail.com>
2020-07-03 16:32:35 +0800

    added styles for layout component

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -4,7 +4,7 @@ import AuthPage from '../AuthPage/AuthPage'
 export default class NameClass extends React.Component{
     render(){
         return(
-            <div>
+            <div className="layout">
                 <AuthPage/>
             </div>
         )
