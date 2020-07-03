37073b02e4301cf85595cb835f703269557bf59a
Bazarov Maxim <bazarow.max@gmail.com>
2020-07-03 23:32:26 +0800

    added styles for contact form

--- src/components/Contacts/Contact/Contact.js
+++ src/components/Contacts/Contact/Contact.js
@@ -9,18 +9,21 @@ class Contact extends React.Component{
         phone: '',
         email: ''
     }
+    _handlerChange(e){
+        this.setState({ [e.target.name] : e.target.value})
+
+        console.log("contact", this.state)
+    }
     _changeContact(e){
-        console.log(e.target.id)
-        const nameValue = document.getElementById(`${e.target.id}name`).innerHTML
-        const phoneValue = document.getElementById(`${e.target.id}phone`).innerHTML
-        const emailValue = document.getElementById(`${e.target.id}email`).innerHTML
+        console.log(e.target.name)
+
         const updatedContact = {
-            id: e.target.id,
-            name: "ujd",
-            phone: phoneValue,
-            email: emailValue 
+            name: this.state.name,
+            phone: this.state.phone,
+            email: this.state.email 
         }
-        fetch(`http://localhost:3000/contacts/${e.target.id}`, {
+ 
+        fetch(`http://localhost:3000/contacts/${e.target.name}`, {
             method: "PUT",
             body: JSON.stringify(updatedContact),
             headers: {
@@ -30,10 +33,11 @@ class Contact extends React.Component{
             .then(response => response.json())
             .then(json => console.log(json))
             .then(() => this._updateContactField())
-            
+            this._toggleForm(e)
+        
     }
     _deleteContact(e) {
-        fetch(`http://localhost:3000/contacts/${e.target.id}`,{
+        fetch(`http://localhost:3000/contacts/${e.target.name}`,{
             method: "DELETE"
         })
         .then(() => this._updateContactField())
@@ -50,23 +54,40 @@ class Contact extends React.Component{
                 }
             )
     }
+    _toggleForm(e){
+        console.log("toggleForm", e.target.name)
+        const form = document.getElementById(`${e.target.name}form`)
+        console.log(form)
+        form.classList.toggle('form_active')
+        this.setState({
+            name: this.props.contact.name,
+            phone: this.props.contact.phone,
+            email: this.props.contact.email
+        })
+    }
     render(){
         console.log(this.props)
         return(
-            <form className="contact">
-
-                    <input value={this.props.contact.name}></input>
-
-
-                    {/* <p id={`${this.props.contact.id}name`}>{this.props.contact.name}</p> */}
+            <div className="contact">
+            <div className="contact__p" >
+                    <p id={`${this.props.contact.id}name`}>{this.props.contact.name}</p> 
                     <p id={`${this.props.contact.id}phone`}>{this.props.contact.phone}</p>
-                    <p id={`${this.props.contact.id}email`}>{this.props.contact.email}</p>
+                    <p id={`${this.props.contact.id}email`}>{this.props.contact.email}</p> 
                     <div className="contact__box-button">
-                        <button id={this.props.contact.id} onClick={(e)=> this._changeContact(e)}>Изменить</button>
-                        <button id={this.props.contact.id} onClick={(e)=> this._deleteContact(e)}>Удалить</button>
+                        <button name={this.props.contact.id} id={`${this.props.contact.id}button`}  onClick={(e)=> this._toggleForm(e)}>Изменить</button>
+                        <button name={this.props.contact.id} onClick={(e)=> this._deleteContact(e)}>Удалить</button>
                     </div>
-                    
+            </div>
+            <form onSubmit={(e)=>e.preventDefault(e)} className="contact__change-form" id={`${this.props.contact.id}form`}>
+                    <input type="text" name="name" onChange={(e)=> this._handlerChange(e)} id={`${this.props.contact.id}name`} value={this.state.name} placeholder={this.props.contact.name}/>
+                    <input type="tel" name="phone" onChange={(e)=> this._handlerChange(e)} id={`${this.props.contact.id}name`} value={this.state.phone} placeholder={this.props.contact.phone}/>
+                    <input type="email" name="email" onChange={(e)=> this._handlerChange(e)} id={`${this.props.contact.id}name`} value={this.state.email} placeholder={this.props.contact.email}/>
+                    <div className="contact__box-button">
+                        <button name={this.props.contact.id} onClick={(e)=> this._changeContact(e)}>Сохранить</button>
+                        <button name={this.props.contact.id} onClick={(e)=> this._toggleForm(e)}>Отмена</button>
+                    </div>
             </form>
+            </div>
         )
     }
 }
